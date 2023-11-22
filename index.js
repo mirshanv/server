69faafaae8056206f26d3a4fc6b9bac78557079d
Mirshan Muhammed <mirshanmuhammed@Mirshans-MacBook-Pro.local>
2023-11-22 12:18:32 +0530

    add api function

--- index.js
+++ index.js
@@ -47,6 +47,33 @@ app.post('/api/login', async (req, res) => {
     }
 });
 
+
+app.post('/api/add-item', async (req, res) => {
+    console.log('====================================');
+    console.log(req);
+    console.log('====================================');
+    try {
+        const { title, price, description, category, image, rating_rate, rating_count } = req.body;
+
+        if (!title || !price || !description || !category || !image || !rating_rate || !rating_count) {
+            return res.status(400).json({ error: 'All fields are required' });
+        }
+
+        const addItemQuery = 'INSERT INTO Products (title, price, description, category, image, rating_rate, rating_count) VALUES (?, ?, ?, ?, ?, ?, ?)';
+        const result = await executeQuery(addItemQuery, [title, price, description, category, image, rating_rate, rating_count]);
+
+        if (result.affectedRows === 1) {
+            res.status(201).json({ message: 'Item added successfully' });
+        } else {
+            res.status(500).json({ error: 'Failed to add item' });
+        }
+    } catch (error) {
+        console.error('Error adding item:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
